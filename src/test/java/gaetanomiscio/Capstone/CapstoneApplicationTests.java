490e96492e14fe0d6ca8e18bc912410f97bc65ff
gaetanopyro <gaetano.miscio@gmail.com>
2025-09-01 12:58:39 +0200

    configuration

---
+++ src/test/java/gaetanomiscio/Capstone/CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package gaetanomiscio.Capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
