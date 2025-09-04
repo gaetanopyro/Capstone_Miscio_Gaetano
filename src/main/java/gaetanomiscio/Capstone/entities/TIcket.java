9ea72fcdc767e8caa93e4fa3225ec9f3b8cef2eb
gaetanopyro <gaetano.miscio@gmail.com>
2025-09-04 17:33:44 +0200

    entity upalod (TIcket,user)

--- src/main/java/gaetanomiscio/Capstone/entities/TIcket.java
+++ src/main/java/gaetanomiscio/Capstone/entities/TIcket.java
@@ -3,19 +3,24 @@ package gaetanomiscio.Capstone.entities;
 import gaetanomiscio.Capstone.enums.Priority;
 import gaetanomiscio.Capstone.enums.Status;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Table(name = "tickets")
+@Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
+@ToString
 
 public class TIcket {
     @Id
+    @GeneratedValue
     private UUID id;
     private String title;
     private String description;
@@ -25,5 +30,11 @@ public class TIcket {
     private Priority priority;
     private LocalDateTime date;
 
-
+    public TIcket(String title, String description, Status status, Priority priority, LocalDateTime date) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.priority = priority;
+        this.date = date;
+    }
 }
--- src/main/java/gaetanomiscio/Capstone/entities/User.java
+++ src/main/java/gaetanomiscio/Capstone/entities/User.java
@@ -1,4 +1,36 @@
 package gaetanomiscio.Capstone.entities;
 
+import gaetanomiscio.Capstone.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+@NoArgsConstructor
+
+
 public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User(String name, String surname, String email, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.role = role.USER;
+    }
 }
