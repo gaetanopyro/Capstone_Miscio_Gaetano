7cd953a23dc5e775869564fda5e86fe20ed11668
gaetanopyro <gaetano.miscio@gmail.com>
2025-09-09 15:24:16 +0200

    add Services

--- src/main/java/gaetanomiscio/Capstone/services/TicketService.java
+++ src/main/java/gaetanomiscio/Capstone/services/TicketService.java
@@ -1,11 +1,65 @@
 package gaetanomiscio.Capstone.services;
 
+import gaetanomiscio.Capstone.entities.Ticket;
+import gaetanomiscio.Capstone.entities.User;
+import gaetanomiscio.Capstone.enums.Status;
+import gaetanomiscio.Capstone.exceptions.NotFoundException;
+import gaetanomiscio.Capstone.payload.CreateTicketDTO;
+import gaetanomiscio.Capstone.payload.UpdateTicketDTO;
 import gaetanomiscio.Capstone.repositories.TicketRepository;
+import gaetanomiscio.Capstone.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TicketService {
     @Autowired
     private TicketRepository ticketRepository;
+    @Autowired
+    private UsersRepository usersRepository;
+
+
+    public Ticket create(CreateTicketDTO nT, UUID userId) {
+        User user = usersRepository.findById(userId).orElseThrow(() -> new NotFoundException(userId));
+        Ticket ticket = new Ticket(
+                nT.title(),
+                nT.description(),
+                nT.status() == null ? Status.OPEN : nT.status(),
+                nT.priority(),
+                LocalDateTime.now(),
+                user
+        );
+        return ticketRepository.save(ticket);
+    }
+
+    public Ticket findById(UUID id) {
+        return ticketRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Ticket con id: " + id + "non trovato."));
+    }
+
+    public List<Ticket> findAllTickets() {
+        return ticketRepository.findAll();
+    }
+
+    public Ticket findByIdAndUpdate(UUID id, UpdateTicketDTO payload) {
+        Ticket found = this.ticketRepository.findById(id).orElseThrow(() -> new NotFoundException("Ticket con id: " + id + "non trovato."));
+        if (payload.description() != null) {
+            found.setDescription(payload.description());
+        }
+        if (payload.status() != null) {
+            found.setStatus(payload.status());
+        }
+        found.setDate(LocalDateTime.now());
+        Ticket modifiedTicket = this.ticketRepository.save(found);
+        return modifiedTicket;
+    }
+
+    public void deleteTicket(UUID id) {
+        Ticket found = this.ticketRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+        this.ticketRepository.delete(found);
+    }
 }
--- src/main/java/gaetanomiscio/Capstone/services/UserService.java
+++ src/main/java/gaetanomiscio/Capstone/services/UserService.java
@@ -1,12 +1,56 @@
 package gaetanomiscio.Capstone.services;
 
+import gaetanomiscio.Capstone.entities.User;
+import gaetanomiscio.Capstone.enums.Role;
+import gaetanomiscio.Capstone.exceptions.NotFoundException;
+import gaetanomiscio.Capstone.payload.UserDTO;
+import gaetanomiscio.Capstone.payload.UserRespDTO;
 import gaetanomiscio.Capstone.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserService {
     @Autowired
     private UsersRepository usersRepository;
+    //private final PasswordEncoder passwordEncoder;
 
+    public User createUser(UserDTO payload) {
+        if (usersRepository.existsByEmail(payload.email())) {
+            throw new IllegalArgumentException("Email già registrata");
+        }
+        User u = new User();
+        u.setUsername(payload.username());
+        u.setEmail(payload.email());
+        // u.setPassword(passwordEncoder.encode(payload.password()));
+        u.setRole(Role.USER);
+
+        return usersRepository.save(u);
+    }
+
+    public User findById(UUID id) {
+        return usersRepository.findById(id).orElseThrow(() -> new NotFoundException("User con id: " + id + "non trovato."));
+    }
+
+    public List<User> findAllUsers() {
+        return usersRepository.findAll();
+    }
+
+    public UserRespDTO updateUser(UUID id, UserDTO payload) {
+        User u = usersRepository.findById(id).orElseThrow(() -> new NotFoundException("User con id: " + id + "non trovato."));
+        u.setUsername(payload.username());
+        u.setEmail(payload.email());
+        //u.setPassword(passwordEncoder.encode(dto.password()));
+        User saved = usersRepository.save(u);
+        return new UserRespDTO(saved.getId());
+    }
+
+
+    public void deleteUser(UUID id) {
+        if (!usersRepository.existsById(id)) throw new NotFoundException(id);
+        usersRepository.deleteById(id);
+    }
 }
