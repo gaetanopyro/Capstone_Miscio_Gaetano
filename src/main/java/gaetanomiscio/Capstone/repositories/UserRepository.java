b0e3736c434c78900053799004a7c8e82ea91e02
gaetanopyro <gaetano.miscio@gmail.com>
2025-09-11 11:56:44 +0200

    add change name repository user

--- src/main/java/gaetanomiscio/Capstone/repositories/UserRepository.java
+++ src/main/java/gaetanomiscio/Capstone/repositories/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UsersRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
